bdbd5c2a357721e1034ae4fe8e76b410ead3e1a8
naoyahirano <hnaoya1120@gmail.com>
2021-09-15 14:04:00 +0900

    first commit

---
+++ frontend/tests/unit/example.spec.js
@@ -0,0 +1,9 @@
+import { mount } from "@vue/test-utils";
+import App from "@/App.vue";
+
+describe("HelloWorld.vue", () => {
+  test("test App Component",function(){
+    const wrapper = mount(App);
+    expect(wrapper.text()).toBe('Hello World')
+  })
+});
