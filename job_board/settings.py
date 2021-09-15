f548841a39457e764e0f80d271642b706ebb8b01
naoyahirano <hnaoya1120@gmail.com>
2021-09-15 22:56:39 +0900

    templatesのindex.htmlとfrontendのApp.vueを共存成功

--- job_board/settings.py
+++ job_board/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'jobs.apps.JobsConfig'
+    'jobs.apps.JobsConfig',
+    'webpack_loader'
 ]
 
 MIDDLEWARE = [
@@ -130,3 +132,10 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+WEBPACK_LOADER = {
+    'DEFAULT' : {
+        'BUNDLE_DIR_NAME': 'dist/',
+        'STATS_FILE': os.path.join(BASE_DIR, 'frontend', 'webpack-stats.json')
+    }
+}
