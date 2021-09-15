f9740f2f223e41f5c8452e54147587326be32fa9
naoyahirano <hnaoya1120@gmail.com>
2021-09-16 00:00:40 +0900

    APIから取得したデータを描画に成功

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,35 @@
 <template>
-  <hello-world />
+  <div>
+    <v-container>
+      <h1>Job Boards</h1>
+      <div v-for="job in jobs" :key="job.pk">
+        <p>{{ job.company_name }}: {{ job.job_title }}</p>
+      </div>
+    </v-container>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
+import { apiService } from '../common/api.service.js'
 
 export default {
-  name: "Home",
-
-  components: {
-    HelloWorld,
+  name: "home",
+  data(){
+    return{
+      jobs: []
+    }
   },
+  methods: {
+    getJobs(){
+      let endPoint = 'api/jobs/'
+      apiService(endPoint).then(data => {
+        this.jobs.push(...data.results)
+      })
+    }
+  },
+  created(){
+    this.getJobs()
+    console.log(this.jobs)
+  }
 };
 </script>
